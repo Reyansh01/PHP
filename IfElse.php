e6ddd2f5138d1b64a6422e4929ba4ace95c56bff
Reyansh Mishra <51244218+Reyansh01@users.noreply.github.com>
2020-05-10 22:42:16 +0530

    If Else and ElseIF in PHP.
    If else in PHP.

---
+++ IfElse.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+
+      $var = 10;
+      if($var == 5)
+      {
+        echo "This is 5.";
+      }
+      else if($var == 10)
+      {
+        echo "This is 10.";
+      }
+      else
+      {
+          echo "This is other than 5 and 10.";
+      }
+    ?>
+
+  </body>
+</html>
